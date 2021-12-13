8e04ae051c585430545e8fc0138c98fb17ec0394
web-dave <dave@webdave.de>
2021-12-13 10:02:16 +0100

    v13 iomport styles in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import * as styles from '../hannes.scss';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'styles-test';
+  constructor() {
+    console.log(styles);
+  }
 }
